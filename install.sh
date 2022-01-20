37e094bf77251dca6b0499557be8efcfec6f3919
Rohit Katakol <rohit@edgeneural.ai>
2022-01-20 16:58:57 +0530

    Changes to install.sh file

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ pip3 install --target=/usr/local/lib/python3.6/dist-packages awsiotsdk-1.0.0.dev
 pip3 install --target=/usr/local/lib/python3.6/dist-packages psutil boto3
 docker pull edgeneural/enapregister:$2
 docker run -it -v /etc/:/tmp/ edgeneural/enapregister:$2 $1
-git clone https://github.com/rohitkatakolen/awsiotsdk.git /home/awsiotsdk
-sudo python3 /home/awsiotsdk/cron_start.py --workdir $1 --hardware $2
+#git clone https://github.com/rohitkatakolen/awsiotsdk.git /home/awsiotsdk
+sudo python3 /tmp/enapjobs/cron_start.py --workdir $1 --hardware $2
 
