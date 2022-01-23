5e60f0b008192376a584f2f27181cd87bf9b610f
edgeneural <rkatakol@gmail.com>
2022-01-24 01:54:51 +0530

    made awsiotsdk whl file installation dynamic with respect to hardware

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-pip3 install --target=/usr/local/lib/python3.6/dist-packages awsiotsdk-1.0.0.dev0-py3-none-any.whl
+pip3 install --target=/usr/local/lib/python3.6/dist-packages awsiotsdk-1.0.0.dev0-py3-$2-any.whl
 pip3 install --target=/usr/local/lib/python3.6/dist-packages psutil boto3
 docker pull edgeneural/enapregister:$2
 docker run -it -v /etc/:/tmp/ edgeneural/enapregister:$2 $1
