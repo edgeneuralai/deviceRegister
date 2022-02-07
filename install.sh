ee7d6de4d1ca9be72b7adec4f4156980a851e1bd
Rohit Katakol <rohit@edgeneural.ai>
2022-02-07 15:18:22 +0530

    Changes to deploy flow

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-pip3 install --target=/usr/local/lib/python3.6/dist-packages awsiotsdk-1.0.0.dev0-py3-$2-any.whl
+pip3 install --target=/usr/local/lib/python3.6/dist-packages awsiotsdk-1.0.0.dev0-py3-$1-any.whl
 pip3 install --target=/usr/local/lib/python3.6/dist-packages psutil boto3
-docker pull edgeneural/enapregister:$2
-docker run -it -v /etc/:/tmp/ edgeneural/enapregister:$2 $1
+docker pull edgeneural/enapregister:$1
+docker run -it -v /etc/:/tmp/ edgeneural/enapregister:$1 $2 $3 $4
 #git clone https://github.com/rohitkatakolen/awsiotsdk.git /home/awsiotsdk
-sudo python3 /tmp/enapjobs/cron_start.py --workdir /tmp/enapjobs/workspace/ --hardware $2
+sudo python3 /tmp/enapjobs/cron_start.py --workdir /tmp/enapjobs/workspace/ --hardware $1
 
