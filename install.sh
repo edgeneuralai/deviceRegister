d0e4d2ae9fbb13c79ab316d0653fc5633633b84a
Rohit Katakol <rohit@edgeneural.ai>
2022-04-08 16:26:47 +0530

    Fixed bugs while registering dockers

--- install.sh
+++ install.sh
@@ -3,9 +3,10 @@ then
 	pip3 install --target=/usr/local/lib/python3.6/dist-packages awsiotsdk-1.0.0.dev0-py3-$1-any.whl
 else
 	pip3 install --target=/usr/local/lib/python3.6/dist-packages awsiotsdk-1.0.0.dev0-py3-jetson-any.whl
-pip3 install --target=/usr/local/lib/python3.6/dist-packages psutil boto3
-docker pull edgeneural/enapregister:$1
-docker run -it -v /etc/:/tmp/ edgeneural/enapregister:$1 $2 $3 $4
+fi
+pip3 install --target=/usr/local/lib/python3.6/dist-packages psutil boto3 crontab
+sudo docker pull edgeneural/enapregister:$1
+sudo docker run -it -v /etc/:/tmp/ edgeneural/enapregister:$1 $2 $3 $4
 #git clone https://github.com/rohitkatakolen/awsiotsdk.git /home/awsiotsdk
 sudo python3 /tmp/enapjobs/cron_start.py --workdir /tmp/enapjobs/workspace/ --hardware $1
 
