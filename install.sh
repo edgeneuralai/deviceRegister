5b0b1ff820b61c7b5d1cfdf0b65e6435a02dcc61
swanandkatdare <katdare.swanand@gmail.com>
2023-02-08 11:45:44 +0530

    hotfix for ubuntu-sv desktop

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 if [ $1=='x86' ]
 then
-	pip3 install awsiotsdk-1.0.0.dev0-py3-none-any.whl
+	python3.6 -m pip install awsiotsdk-1.0.0.dev0-py3-none-any.whl
 else
-	pip3 install awsiotsdk-1.0.0.dev0-py3-jetson-any.whl
+	python3.6 -m pip install awsiotsdk-1.0.0.dev0-py3-jetson-any.whl
 fi
-pip3 install psutil boto3 docker
+python3.6 -m pip  install psutil boto3 docker
 
 sudo docker pull edgeneural/enapregister:$1
 sudo docker run -it -v /etc/:/tmp/ edgeneural/enapregister:$1 $2 $3 $4 $5
@@ -12,8 +12,8 @@ sudo docker run -it -v /etc/:/tmp/ edgeneural/enapregister:$1 $2 $3 $4 $5
 #sudo python3 /tmp/enapjobs/cron_start.py --workdir /tmp/enapjobs/workspace/ --hardware $1
 if [ $1=='x86' ]
 then
-        sudo python3 samples/jobs_infer.py --config /etc/permanent_cert/perm_config.ini --workdir /tmp/enapjobs/workspace/ --hardware $1
+        sudo python3.6 samples/jobs_infer.py --config /etc/permanent_cert/perm_config.ini --workdir /tmp/enapjobs/workspace/ --hardware $1
 else
-	sudo python3 samples/jobs_infer.py --config /etc/permanent_cert/perm_config.ini --workdir /tmp/enapjobs/workspace/ --hardware jetson
+	sudo python3.6 samples/jobs_infer.py --config /etc/permanent_cert/perm_config.ini --workdir /tmp/enapjobs/workspace/ --hardware jetson
 fi
 
